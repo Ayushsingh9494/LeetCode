ba3ac247492c77bc52aed07bfbe8c3789feac2ae
Ayush Singh <150294191+Ayushsingh9494@users.noreply.github.com>
2026-02-09 09:55:19 +0530

    Time: 4 ms (24.33%) | Memory: 12.2 MB (96.58%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> st;
+        vector<int> greater(nums2.size(),-1);
+        for(int i=nums2.size()-1;i>=0;i--){
+            while(!st.empty() && nums2[i]>=st.top()){
+                st.pop();
+            }
+
+            if(st.empty()) greater[i] = -1;
+            else greater[i] = st.top();
+
+            st.push(nums2[i]);
+        }
+
+        for(int i=0;i<nums1.size();i++){
+            auto idx = find(nums2.begin(),nums2.end(),nums1[i]);
+            int id = idx - nums2.begin();
+            nums1[i] = greater[id];
+        }
+        return nums1;
+    }
+};
+
+
+
+
+
+
+
+// int n1 = nums1.size();
+//         int n2 = nums2.size();
+//         vector<int> res;
+//         for(int i=0;i<n1;i++){
+//             int greater = -1;
+//             auto it = find(nums2.begin(),nums2.end(),nums1[i]);
+//             if(it!=nums2.end()){
+//                 size_t idx = it - nums2.begin();
+//                 for(int j=idx;j<n2;j++){
+//                     if(nums2[j]>nums1[i]){
+//                         greater = nums2[j];
+//                         break;
+//                     }
+//                 }
+//             }
+//             res.push_back(greater);
+//         }
+//         return res;
