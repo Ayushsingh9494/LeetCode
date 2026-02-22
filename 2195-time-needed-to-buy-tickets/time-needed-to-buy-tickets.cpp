f9e99b743593b6af35e248777d1317d103a3f65c
Ayush Singh <150294191+Ayushsingh9494@users.noreply.github.com>
2026-02-22 06:18:04 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (82.52%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
-        int n=tickets.size();
-        queue<int> q;
-        int time = 0;
-        for(int i=0;i<n;i++){
-            q.push(i);
-        }
-        while(!q.empty()){
-            int curr = q.front();
-            q.pop();
-            tickets[curr]--;
-            time++;
-            if(curr==k && tickets[curr]==0) return time;
+        // int n=tickets.size();
+        // queue<int> q;
+        // int time = 0;
+        // for(int i=0;i<n;i++){
+        //     q.push(i);
+        // }
+        // while(!q.empty()){
+        //     int curr = q.front();
+        //     q.pop();
+        //     tickets[curr]--;
+        //     time++;
+        //     if(curr==k && tickets[curr]==0) return time;
 
-            if(tickets[curr]!=0) q.push(curr);
-        }
+        //     if(tickets[curr]!=0) q.push(curr);
+        // }
 
-        return time;
+        // return time;
+        int value = tickets[k];
+        int seconds = 0;
+        for(int i = 0; i<tickets.size(); i++) {
+            int max = (i<=k)? value: value-1;
+            seconds+= min(tickets[i],max);
+        }
+        return seconds;
     }
 };
