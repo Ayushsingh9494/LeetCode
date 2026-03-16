3a767cc1bf63f4e5b3150b119059ba132da82b80
Ayush Singh <150294191+Ayushsingh9494@users.noreply.github.com>
2026-03-16 19:01:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (83.63%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,36 +1,57 @@
 class Solution {
+    private:
+    void dfs(int v,vector<vector<int>>& isConnected,vector<int> &visited){
+        visited[v]  = 1;
+        for(int j=0;j<isConnected.size();j++){
+            if(isConnected[v][j] && !visited[j]){
+                visited[j] = 1;
+                dfs(j,isConnected,visited);
+            }
+        }
+    }
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int v = isConnected.size();
-        vector<vector<int>> adj(v);
         vector<int> visited(v);
-        for(int i=0;i<v;i++){
-            for(int j=0;j<v;j++){
-                if(isConnected[i][j] && i!=j){
-                    adj[i].push_back(j);
-                }
-            }
-        }
-
         int cnt = 0;
         for(int i=0;i<v;i++){
             if(!visited[i]){
                 cnt++;
-                queue<int> q;
-                q.push(i);
-                visited[i] = 1;
-                while(!q.empty()){
-                    int n = q.front();
-                    q.pop();
-                    for(auto &nbr: adj[n]){
-                        if(!visited[nbr]){
-                            visited[nbr] = 1;
-                            q.push(nbr);
-                        }
-                    }
-                }
+                dfs(i,isConnected,visited);
             }
         }
         return cnt;
+    // --------------bfs---------------
+    //     int v = isConnected.size();
+    //     vector<vector<int>> adj(v);
+    //     vector<int> visited(v);
+    //     for(int i=0;i<v;i++){
+    //         for(int j=0;j<v;j++){
+    //             if(isConnected[i][j] && i!=j){
+    //                 adj[i].push_back(j);
+    //             }
+    //         }
+    //     }
+
+    //     int cnt = 0;
+    //     for(int i=0;i<v;i++){
+    //         if(!visited[i]){
+    //             cnt++;
+    //             queue<int> q;
+    //             q.push(i);
+    //             visited[i] = 1;
+    //             while(!q.empty()){
+    //                 int n = q.front();
+    //                 q.pop();
+    //                 for(auto &nbr: adj[n]){
+    //                     if(!visited[nbr]){
+    //                         visited[nbr] = 1;
+    //                         q.push(nbr);
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return cnt;
     }
 };
