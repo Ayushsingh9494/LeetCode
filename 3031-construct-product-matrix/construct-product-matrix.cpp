fc3ec4f49f333a2eba9a6f7ecbc6e177cdf9bf97
Ayush Singh <150294191+Ayushsingh9494@users.noreply.github.com>
2026-03-24 19:17:20 +0530

    Time: 32 ms (76.02%) | Memory: 146.6 MB (32.84%) - LeetSync

--- 3031-construct-product-matrix/construct-product-matrix.cpp
+++ 3031-construct-product-matrix/construct-product-matrix.cpp
@@ -4,6 +4,7 @@ public:
         const int MOD = 12345;
         int m = grid.size();
         int n = grid[0].size();
+        vector<vector<int>> ans(m,vector<int>(n));
         long long pref=1;
         long long suff = 1;
         vector<int> single;
@@ -14,21 +15,19 @@ public:
         }
         vector<long long> suffix(m*n);
         vector<long long> prefix(m*n);
-        for(int i=0;i<m*n;i++){
-            prefix[i] = pref;
-            pref = single[i]*pref%MOD;
-        }
-        for(int i=m*n-1;i>=0;i--){
-            suffix[i] = suff;
-            suff = single[i]*suff%MOD;
-        }
-        int idx=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                grid[i][j] = prefix[idx] * suffix[idx] % MOD;
-                idx++;
+                ans[i][j] = pref;
+                pref = grid[i][j]*pref%MOD;
             }
         }
-        return grid;
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                ans[i][j] = ans[i][j] * suff % MOD;
+                suff = grid[i][j]* suff % MOD;
+            }
+        }
+        
+        return ans;
     }
 };
