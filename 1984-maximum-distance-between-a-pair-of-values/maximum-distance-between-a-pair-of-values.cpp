43bd52f449df3132b0bdfb4375a28398c2b486c9
Ayush Singh <150294191+Ayushsingh9494@users.noreply.github.com>
2026-04-19 17:56:49 +0530

    Time: 3 ms (55.59%) | Memory: 102.3 MB (47.28%) - LeetSync

--- 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.cpp
+++ 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.cpp
@@ -1,32 +1,42 @@
 class Solution {
-    private:
-    int upperBound(vector<int> &arr,int target){
-    int n = arr.size();
-    int low = 0;
-    int high = n-1;
-    int idx = -1;
-    while(low<=high){
-        int mid = low + (high-low)/2;
-        if(arr[mid]>=target){
-            idx = mid;
-            low = mid+1;
-        }
-        else{
-            high = mid-1;
-        }
-    }
-    return idx;
-    }
+    // private:
+    // int upperBound(vector<int> &arr,int target){
+    // int n = arr.size();
+    // int low = 0;
+    // int high = n-1;
+    // int idx = -1;
+    // while(low<=high){
+    //     int mid = low + (high-low)/2;
+    //     if(arr[mid]>=target){
+    //         idx = mid;
+    //         low = mid+1;
+    //     }
+    //     else{
+    //         high = mid-1;
+    //     }
+    // }
+    // return idx;
+    // }
 public:
     int maxDistance(vector<int>& nums1, vector<int>& nums2) {
-        vector<pair<int,int>> res;
         int n1 = nums1.size();
         int n2 = nums2.size();
-        int maxD = 0;
+        int i = 0;
+        int j = 0;
+        int ans = 0;
 
-        for(int i=0;i<n1;i++){
-            maxD = max(maxD,upperBound(nums2,nums1[i])-i);
+        // two - pointers approach
+
+        while(i<n1 && j<n2){
+            if(nums1[i]<=nums2[j]){
+                ans = max(ans,j-i);
+                j++;
+            }
+            else{
+                i++;
+                if(i>j) j = i;
+            }
         }
-        return maxD;
+        return ans;
     }
 };
